a86fe4b481a89aa038bd5ff805ce9dbfd516b191
mzabolotnev <mzabolotnev@liveperson.com>
2021-07-07 12:48:52 +0300

    Add file-list module
    Add file-list.component and connect it to store

--- src/app/app.store.ts
+++ src/app/app.store.ts
@@ -1,14 +1,17 @@
 import * as fromController from './controller/store/controller.reducer';
+import * as fromFileList from './file-list/store/file-list.reducer';
 import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
 import { storeLogger } from 'ngrx-store-logger';
 
 
 export interface StoreModel {
   controller: fromController.State;
+  fileList: fromFileList.State;
 }
 
 export const reducers: ActionReducerMap<StoreModel> = {
   controller: fromController.reducer,
+  fileList: fromFileList.reducer,
 };
 
 export function logger(reducer: ActionReducer<StoreModel>): ActionReducer<StoreModel> {
